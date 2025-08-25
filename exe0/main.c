6d227d101ec52e215066166dd42e1fd80d9f1e49
Matheusconst13 <matheuscsc1@al.insper.edu.br>
2025-08-25 13:37:42 -0300

    ex0

--- exe0/main.c
+++ exe0/main.c
@@ -12,17 +12,6 @@ int btn_flag;
 
 void btn_callback(uint gpio, uint32_t events) {
   if (events == 0x4) { // fall edge
-
-    printf("btn pressed \n");
-
-    while (!gpio_get(BTN_PIN_R)) {
-      sleep_ms(1);
-    }
-
-
-    printf("btn released \n");
-
-    sleep_ms(1);
     btn_flag = 1;
   }
 }
@@ -35,16 +24,11 @@ int main() {
   gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true,
                                      &btn_callback);
 
-  volatile int capture_flag = 0;
-  int a;
   while (1) {
-    if (btn_flag) {
-      capture_flag = 1;
+    if (btn_flag)
+    {
       btn_flag = 0;
+      printf("btn pressed \n");
     }
-
-    if (capture_flag) {
-    }
-
   }
 }
