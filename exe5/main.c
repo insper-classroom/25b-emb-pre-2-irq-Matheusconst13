f3182b5fd0d8ffb701edc62307728a231c3dee94
Matheusconst13 <matheuscsc1@al.insper.edu.br>
2025-08-25 15:16:18 -0300

    ex5.3

--- exe5/main.c
+++ exe5/main.c
@@ -9,19 +9,16 @@ const int BTN_PIN = 28;
 uint32_t tempo_entrada = 0;
 uint32_t tempo_saida = 0;
 
-// Função de interrupção do botão
 void btn_callback(uint gpio, uint32_t events) {
-  if (events == 0x4) { // Quando o botão for pressionado (falling edge)
     if (gpio == BTN_PIN) {
-      tempo_entrada = time_us_32();  // Registra o tempo de entrada
-      flag_entrada = 1;
+        if (events == GPIO_IRQ_EDGE_FALL) { 
+            tempo_entrada = time_us_32();  
+            flag_entrada = 1;
+        } else if (events == GPIO_IRQ_EDGE_RISE) {
+            tempo_saida = time_us_32();  
+            flag_saida = 1;
+        }
     }
-  } else if (events == 0x8) { // Quando o botão for liberado (rising edge)
-    if (gpio == BTN_PIN) {
-      tempo_saida = time_us_32();  // Registra o tempo de saída
-      flag_saida = 1;
-    }
-  }
 }
 
 int main() {
@@ -31,21 +28,19 @@ int main() {
     gpio_set_dir(BTN_PIN, GPIO_IN);
     gpio_pull_up(BTN_PIN);
 
-
     gpio_set_irq_enabled_with_callback(
         BTN_PIN, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &btn_callback);
 
     while (true) {
         if (flag_entrada) {
             flag_entrada = 0;
-            while (!flag_saida) {}
+            while (!flag_saida) {} 
 
             flag_saida = 0;
 
-            uint32_t duracao = tempo_saida - tempo_entrada; 
+            uint32_t duracao = tempo_saida - tempo_entrada;
 
             if (duracao > 800000) { 
-                
                 printf("Aperto longo!\n");
             } else {
                 printf("Aperto curto!\n");
